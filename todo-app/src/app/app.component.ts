3921d293af59ee4ceff5431a009fe321a61b08d1
Arno <sintes.arnaud@gmail.com>
2019-04-03 22:43:05 +0200

    Done up to 'Creating the TodoListComponent:Notice that we’ve introduced'

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -10,21 +10,28 @@ import {TodoDataService} from './todo-data.service';
 })
 export class AppComponent {
 
-  newTodo: Todo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // newTodo: Todo = new Todo();
 
   constructor(private todoDataService: TodoDataService) {
   }
 
-  addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
+
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
   }
 
-  toggleTodoComplete(todo) {
+  toggleTodoComplete(todo: Todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo) {
+  removeTodo(todo: Todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
 
--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
